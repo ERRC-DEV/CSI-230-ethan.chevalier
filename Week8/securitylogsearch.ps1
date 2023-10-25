dbabf908055b18bd63c622c93fcfd0c4da4b6cc2
ERRC-DEV <ethan.chevalier@mymail.champlain.edu>
2023-10-25 17:22:25 -0400

    Week 8 finished

---
+++ Week8/securitylogsearch.ps1
@@ -0,0 +1,14 @@
+﻿# Review security event log
+
+$myDir = "C:\Users\champuser\Desktop"
+
+# List logs
+Get-EventLog -list
+
+# Prompt user for selecting what log to view
+$whichLog = Read-host -Prompt "Please select which log to review from the above list"
+$searchString = Read-host -Prompt "Please enter the string you would like to search for"
+
+# Print results
+Get-EventLog -Logname $whichlog -Newest 40 | where {$_.Message -ilike "*$searchString*"} | export-csv -NoTypeInformation `
+-Path "$myDir\securityLogs.csv"
