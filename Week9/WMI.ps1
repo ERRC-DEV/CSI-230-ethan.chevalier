548f6a4fa88aab55865c353f67a9dda1d1e1f68f
ERRC-DEV <ethan.chevalier@mymail.champlain.edu>
2023-11-01 13:43:20 -0400

    Week 9 done

---
+++ Week9/WMI.ps1
@@ -0,0 +1,16 @@
+﻿$myDir = "C:\Users\champuser\Desktop"
+
+#GETTING IPS
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration
+
+# Get and export running services
+Read-host -Prompt "The running services segment begins here"
+Get-WmiObject -Class Win32_Service -Filter "state='Running'" | export-csv -NoTypeInformation -Path "$myDir\runningServices.csv"
+Get-WmiObject -Class Win32_Service -Filter "state='Running'"
+
+# CALCULATOR
+Read-host -Prompt "The calculator segment begins here"
+Start-Process win32calc.exe
+Read-host
+$p = Get-Process -Name win32calc
+Stop-Process -InputObject $p
