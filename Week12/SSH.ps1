d75f4597de85f2225793fdd924165d84864b1050
ERRC-DEV <ethan.chevalier@mymail.champlain.edu>
2023-11-28 19:28:43 -0500

    Week 12 done

---
+++ Week12/SSH.ps1
@@ -0,0 +1,16 @@
+﻿#New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-Credential sys320)
+$running = true
+while ($running)
+{
+    $the_cmd = Read-Host -Prompt "Please enter a command"
+    
+    (Invoke-SSHCommand -index 0 $the_cmd).Output
+
+    if ($the_cmd -eq "exit")
+    {
+        $running = false
+    }
+}
+
+Set-SCPFile -ComputerName '192.168.4.22' -Credential (Get-Credential sys320) `
+-RemotePath '/home/champuser' -LocalFile '.\test.txt'
